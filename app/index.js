72080087813ae6fa2987454723e41b68ab96bd6f
David Hughes <david.hughes125@gmail.com>
2014-04-23 23:06:15 +0100

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,64 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+
+var DrupalModuleGenerator = yeoman.generators.Base.extend({
+  init: function () {
+    this.pkg = require('../package.json');
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.installDependencies();
+      }
+    });
+  },
+
+  askFor: function () {
+    var done = this.async();
+
+    // have Yeoman greet the user
+    this.log(this.yeoman);
+
+    // replace it with a short and sweet description of your generator
+    this.log(chalk.magenta('You\'re using the fantastic DrupalModule generator.'));
+
+    var prompts = [{
+      name: 'moduleName',
+      message: 'What should I name your module?'
+    }, {
+      type: 'confirm',
+      name: 'scripts',
+      message: 'Does your module need any JavaScript?',
+      default: true
+    }, {
+      type: 'confirm',
+      name: 'sass',
+      message: 'Does your module need to use Sass?',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.someOption = props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  app: function () {
+    this.mkdir('app');
+    this.mkdir('app/templates');
+
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
+  },
+
+  projectfiles: function () {
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
+  }
+});
+
+module.exports = DrupalModuleGenerator;
