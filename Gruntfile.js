20afa5ed7b0045dfb9a141900673e85b81101bab
David Hughes <david.hughes125@gmail.com>
2014-04-24 12:36:49 +0100

    Update jshintrc settings and enable its usage in Grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,10 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-        all: ['Gruntfile.js', 'app/*.js']
+        all: ['Gruntfile.js', 'app/*.js'],
+        options: {
+          jshintrc: '.jshintrc'
+        }
     }
   });
 
