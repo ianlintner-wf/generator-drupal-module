c32c5fc96add764dd83a2c0c472fc22b19ae07d4
David Hughes <david.hughes125@gmail.com>
2014-04-24 17:52:11 +0100

    Second test scenario, mostly to trigger Travis CI

--- test/test-creation.js
+++ test/test-creation.js
@@ -17,7 +17,7 @@ describe('drupal-module generator', function () {
     }.bind(this));
   });
 
-  it('creates expected files', function (done) {
+  it('creates all expected files', function (done) {
     var expected = [
       // add files you expect to exist here.
       'Gruntfile.js',
@@ -27,6 +27,29 @@ describe('drupal-module generator', function () {
 
     helpers.mockPrompt(this.app, {
       'addScripts': true,
+      'addSass': true,
+      'moduleName': 'testModule'
+    });
+    this.app.options['skip-install'] = true;
+    try {
+      this.app.run({}, function () {
+        helpers.assertFile(expected);
+        done();
+      });
+    } catch (e) {
+      fail(e.getMessage());
+    }
+  });
+  it('generates without JS scripts', function (done) {
+    var expected = [
+      // add files you expect to exist here.
+      'Gruntfile.js',
+      'package.json',
+      'cruk-test.info'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      'addScripts': false,
       'addSass': true,
       'moduleName': 'testModule'
     });
