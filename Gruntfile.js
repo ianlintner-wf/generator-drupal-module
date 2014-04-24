231347d447b3c3538ec6b4e567305bffbcc69bd4
David Hughes <david.hughes125@gmail.com>
2014-04-24 12:25:34 +0100

    Gruntify

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+        all: ['Gruntfile.js', 'app/*.js']
+    }
+  });
+
+// Load the plugin that provides the tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+// Default task(s).
+  grunt.registerTask('default', ['jshint']);
+};
