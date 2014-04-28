5eed7cc31e0cdefa0b8836ddb7512dd763aad178
David Hughes <david.hughes125@gmail.com>
2014-04-28 16:22:23 +0100

    Add template files for CSS/SASS

---
+++ app/templates/config.rb
@@ -0,0 +1,14 @@
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "scripts"
+
+preferred_syntax = <%= css_syntax %>
+disable_warnings = true
+
+environment = :development
+line_comments = (enviroment == :production) ? false : false
+output_style = (environment == :production) ? :compressed : :expanded
